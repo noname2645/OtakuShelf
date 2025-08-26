0937e806be7ac1818df8634062536938283080f2
ROHIT KARMOKAR <rohitkarmokar732@gmail.com>
2025-08-27 00:46:44 +0530

    login/reg

--- otakushelf/src/Frontend/App.jsx
+++ otakushelf/src/Frontend/App.jsx
@@ -1,11 +1,33 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react'; 
 import './App.css'
 import Home from "../Frontend/components/home.jsx"
+import List from "../Frontend/components/list.jsx"
+import Login from "../Frontend/components/login.jsx"
+import Register from "../Frontend/components/register.jsx"
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/list",
+      element: <List />,
+    },
+       {
+      path: "/login",
+      element: <Login />,
+    },
+       {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />
     </>
   )
 }
